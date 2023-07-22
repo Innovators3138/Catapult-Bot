0ade3e62dd1e5de74fcb774a9462f42ebd753acf
Michael Hill <michael@Michaels-MacBook-Air.local>
2023-07-22 10:17:37 -0400

    convert to double solenoid

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -25,7 +25,8 @@ public final class Constants {
   public static final double DeadzoneDefault = 0.03;
 
   //Solenoid Configuration IDs
-  public static final int launcherSolenoidChannel = 0;
+  public static final int launcherSolenoidOutChannel = 0;
+  public static final int launcherSolenoidInChannel = 1;
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
